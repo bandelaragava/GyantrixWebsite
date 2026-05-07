fb1eb4bcdc63bd7a46301c8fc02112e552206fe9
Niharika <tsai36369@gmail.com>
2026-05-07 23:52:05 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.css';
 import Navbar from './Navbar';
 import AnnouncementTicker from './AnnouncementTicker';
 
 const Header = () => {
+    const [isFixed, setIsFixed] = useState(false);
+
     useEffect(() => {
-        // Scroll logic removed as navbar should remain sticky and not move
+        const handleScroll = () => {
+            // The red announcement bar is ~40px. When we scroll past it, fix the navbar.
+            if (window.scrollY > 40) {
+                setIsFixed(true);
+            } else {
+                setIsFixed(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        // Check initial position on load
+        handleScroll();
+
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
-        <div className="site-header">
+        <>
             <AnnouncementTicker />
-            <Navbar />
-        </div>
+            <div className={`site-header ${isFixed ? 'fixed-header' : ''}`}>
+                <Navbar />
+            </div>
+            {/* Prevents page content from jumping abruptly when navbar becomes fixed */}
+            {isFixed && <div className="header-spacer" style={{ height: '70px', width: '100%' }}></div>}
+        </>
     );
 };
 
